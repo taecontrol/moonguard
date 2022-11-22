4518b35a8dfcadca9f711d61a33342b5a709f7d0
Luis Güette <guette@kirschbaumdevelopment.com>
2022-11-21 21:04:15 -0400

    Create package

---
+++ src/LarastatsServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Taecontrol\Larastats;
+
+use Illuminate\Support\ServiceProvider;
+
+class LarastatsServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+}
