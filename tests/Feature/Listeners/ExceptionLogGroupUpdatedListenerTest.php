a066924a39a126b0236d5238ff8c1b930195af05
Ronald Pereira <rpereira@taecontrol.com>
2022-12-01 16:02:30 -0400

    [#1] added tests

---
+++ tests/Feature/Listeners/ExceptionLogGroupUpdatedListenerTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Taecontrol\Larastats\Tests\Feature\Controllers;
+
+use Taecontrol\Larastats\Models\User;
+use Taecontrol\Larastats\Tests\TestCase;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Notification;
+use Taecontrol\Larastats\Models\ExceptionLogGroup;
+use Taecontrol\Larastats\Events\ExceptionLogGroupCreatedEvent;
+use Taecontrol\Larastats\Events\ExceptionLogGroupUpdatedEvent;
+use Taecontrol\Larastats\Listeners\ExceptionLogGroupCreatedListener;
+use Taecontrol\Larastats\Listeners\ExceptionLogGroupUpdatedListener;
+use Taecontrol\Larastats\Notifications\NewExceptionLogGroupNotification;
+
+class ExceptionLogGroupUpdatedListenerTest extends TestCase
+{
+    /** @var Collection<User> */
+    protected Collection $users;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->users = User::factory()->count(2)->create();
+    }
+
+    /** @test */
+    public function listener_sends_notification_when_an_exception_log_group_is_created()
+    {
+        Notification::fake();
+        $instance = new ExceptionLogGroupUpdatedListener();
+
+        $exceptionLogGroup = ExceptionLogGroup::factory()->create();
+        $event = new ExceptionLogGroupUpdatedEvent($exceptionLogGroup);
+        $instance->handle($event);
+
+        Notification::assertSentTo($this->users, NewExceptionLogGroupNotification::class);
+    }
+}
