7b63203d6c281f212531e7728b7cd21c38077efe
w1ls0nv3l <wilsonvelasco96@gmail.com>
2022-12-14 11:31:49 -0400

    [FEAT-28] Create class LarastatsCommandsScheduler

---
+++ src/Console/LarastatsCommandsScheduler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Taecontrol\Larastats\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Taecontrol\Larastats\Console\Commands\CheckUptimeCommand;
+use Taecontrol\Larastats\Console\Commands\CheckSslCertificateCommand;
+
+class LarastatsCommandScheduler
+{
+    public static function scheduleLarastatsCommands(Schedule $schedule, string $uptimeCheckCron, string $sslCertificateCheckCron)
+    {
+        /** @var bool $uptimeCheckIsEnabled */
+        $uptimeCheckIsEnabled = config('larastats.uptime_check.enabled');
+        /** @var bool $sslCheckIsEnabled */
+        $sslCheckIsEnabled = config('larastats.ssl_certificate_check.enabled');
+
+        if ($uptimeCheckIsEnabled) {
+            $schedule->command(CheckUptimeCommand::class)
+                ->cron($uptimeCheckCron);
+        }
+
+        if ($sslCheckIsEnabled) {
+            $schedule->command(CheckSslCertificateCommand::class)
+                ->cron($sslCertificateCheckCron);
+        }
+    }
+}
